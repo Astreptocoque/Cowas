39e58ca1fe312ef1800483517ffdb53fe0051811
Timothée Hirt <astreptocoque@gmail.com>
2022-01-07 18:12:58 +0100

    added GPIO object

---
+++ include/GPIO.h
@@ -0,0 +1,15 @@
+#ifndef GPIO_H
+#define GPIO_H
+#include <Arduino.h>
+#include "interfaces/GPIO_interface.h"
+
+class GPIO : public GPIO_interface
+{
+  public:
+    void begin(byte _pin, byte _mode);
+    void begin(byte _pin, byte _mode, String _ID);
+    void on();
+    void off();
+    void read();
+};
+#endif
